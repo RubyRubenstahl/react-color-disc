face01ec6bd742881a190ec01b721bf641f54f05
Ruby Rubenstahl <ruby@ruby-lighting.com>
2018-06-17 22:27:57 -0400

    Got marker displaying to the correct color on the wheel

--- src/ColorWheel.js
+++ src/ColorWheel.js
@@ -1,15 +1,40 @@
 import React from "react";
 import propTypes from "prop-types";
+import Konva from "konva";
+import { Stage, Image, Layer, Circle } from "react-konva";
+import Wheel from "./components/Wheel";
+import colorToCartesian from './helpers/colorToCartesian';
 
-const ColorWheel = props => {
-  const {} = props;
-
-  return (<div>ColorWheel</div>);
-
-}
+class ColorWheel extends React.Component{
+  state={color: "white"}
+  render(){
+  const { radius = 150, color = "yellow" } = this.props;
+  const markerPos = colorToCartesian(color, radius);
+  return (
+    <Stage width={radius * 2} height={radius * 2}>
+      <Layer>
+        <Wheel radius={radius} />
+      </Layer>
+      <Layer>
+        <Circle
+          x={markerPos.x}
+          y={markerPos.y}
+          width={15}
+          height={15}
+          strokeWidth={2}
+          stroke={"#AAA"}
+          shadowOffset={{x:1, y:1}}
+          shadowOpacity={.3}
+        />
+      </Layer>
+    </Stage>
+  )
+  }
+};
 
 ColorWheel.propTypes = {
-  color: propTypes.string.isRequired
+  color: propTypes.string,
+  radius: propTypes.number
 };
 
 export default ColorWheel;
