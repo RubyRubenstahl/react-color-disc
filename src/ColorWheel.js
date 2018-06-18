d2623de3e707ed6ef4a3ca4b715ee4d419f09c3a
Ruby Rubenstahl <ruby@ruby-lighting.com>
2018-06-18 00:39:37 -0400

    Extracted marker to a separate component

--- src/ColorWheel.js
+++ src/ColorWheel.js
@@ -3,34 +3,33 @@ import propTypes from "prop-types";
 import Konva from "konva";
 import { Stage, Image, Layer, Circle } from "react-konva";
 import Wheel from "./components/Wheel";
-import colorToCartesian from './helpers/colorToCartesian';
+import colorToCartesian from "./helpers/colorToCartesian";
+import Marker from "./components/Marker";
+class ColorWheel extends React.Component {
+  state = { color: "white" };
 
-class ColorWheel extends React.Component{
-  state={color: "white"}
-  render(){
-  const { radius = 150, color = "yellow" } = this.props;
-  const markerPos = colorToCartesian(color, radius);
-  return (
-    <Stage width={radius * 2} height={radius * 2}>
-      <Layer>
-        <Wheel radius={radius} />
-      </Layer>
-      <Layer>
-        <Circle
-          x={markerPos.x}
-          y={markerPos.y}
-          width={15}
-          height={15}
-          strokeWidth={2}
-          stroke={"#AAA"}
-          shadowOffset={{x:1, y:1}}
-          shadowOpacity={.3}
-        />
-      </Layer>
-    </Stage>
-  )
+  handleClick = e => {
+    debugger
+    return e;
+  };
+
+
+  render() {
+    const { radius = 150, color = "yellow" } = this.props;
+
+    const markerPos = colorToCartesian(color, radius);
+    return (
+      <Stage width={radius * 2} height={radius * 2}>
+        <Layer>
+          <Wheel radius={radius} onClick={this.handleClick} />
+        </Layer>
+        <Layer>
+          <Marker pos={markerPos} />
+        </Layer>
+      </Stage>
+    );
   }
-};
+}
 
 ColorWheel.propTypes = {
   color: propTypes.string,
---
+++ src/components/Marker.js
@@ -0,0 +1,39 @@
+import React from "react";
+import propTypes from "prop-types";
+import Konva from "konva";
+import { Stage, Image, Layer, Circle } from "react-konva";
+
+function Marker({
+  pos,
+  radius = 15,
+  strokeWidth = 2,
+  strokeColor = "#AAA",
+  shadowOffset = { x: 1, y: 1 },
+  shadowOpacity = 0.3,
+    ...rest
+}) {
+  return(
+    <Circle
+      x={pos.x}
+      y={pos.y}
+      width={radius}
+      height={radius}
+      strokeWidth={strokeWidth}
+      stroke={strokeColor}
+      shadowOffset={shadowOffset}
+      shadowOpacity={shadowOpacity}
+      {...rest}
+    />
+  );
+}
+
+Marker.propTypes={
+  pos: propTypes.number,
+  radius: propTypes.number,
+  strokeWidth: propTypes.number,
+  strokeColor: propTypes.string,
+  shadowOffset: propTypes.number,
+  shadowOpacity: propTypes.number,
+}
+
+export default Marker;
